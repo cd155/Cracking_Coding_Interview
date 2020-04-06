f5c36713697097e558fefa4a14c62047da6a513b
Don <cd155@163.com>
2020-04-06 15:03:12 +0800

    Finished the 1.1 solution

--- 1_Arrays_and_Strings/1_1_Is_Unique.py
+++ 1_Arrays_and_Strings/1_1_Is_Unique.py
@@ -2,19 +2,38 @@
 # Implement an algorithm to determine if a string has all unique characters. 
 # What if you cannot use additional data structures?
 
-test_string = "piegie"
-dict_table = {}
+test_array = ["algorithm", "", "additional", "h", "2020", "DonaldTrump", "Tt", "12ad好"]
 
 def is_unique_hash(test_string):
+    dict_table = {}
     for char in test_string:
         if char in dict_table:
-            print("Find the duplicate")
+            print ("Hash: Find the duplicate in " + test_string)
             return
         else:
             dict_table[char] = True
-    print ("No duplicate")
+    print ("Hash: No duplicate in " + test_string)
 
 def is_unique_array(test_string):
+    bool_array = [False] * 128
+    for char in test_string:
+        if ord(char) > 128:
+            print ("Array: Out of Assumption in " + test_string)
+            return
+
+        if bool_array[ord(char)]:
+            print ("Array: Find the duplicate in " + test_string)
+            return
+        else:
+            bool_array[ord(char)] = True
+    print ("Array: No duplicate in " + test_string)
+
+def is_unique_bit_array(test_string):
+    # check the following if you have some trouble to understand the bit vector solution
+    # https://stackoverflow.com/questions/9141830/explain-the-use-of-a-bit-vector-for-determining-if-all-characters-are-unique
     pass
 
-is_unique(test_string)
+# for test only
+for test_string in test_array:
+    is_unique_hash(test_string)
+    is_unique_array(test_string)
