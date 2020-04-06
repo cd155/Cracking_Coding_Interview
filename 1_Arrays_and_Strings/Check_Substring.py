a2f132b7069284331f5bbc8347c2caec8469467f
Don <cd155@163.com>
2020-04-06 23:27:19 +0800

    change file name to checksubstring

--- 1_Arrays_and_Strings/Check_Substring.py
+++ 1_Arrays_and_Strings/Check_Substring.py
@@ -1,7 +1,7 @@
-# Check Permutation: 
-# Given two strings,write a method to decide if one is a permutation of the other.
+# Check Substring: 
+# Given two strings,write a method to decide if one is a substring of the other.
 
-def check_permutation(string_one, string_two):
+def check_substring(string_one, string_two):
     if len(string_one) > len(string_two):
         return pair_string(string_one, string_two)
     else:
@@ -20,7 +20,7 @@ def pair_string(string_long, string_short):
 # for test only
 test_arrays = [["abbcaaade","aaa"],["",""],["thisis","is"],["","ab"],["a","b"]]
 for item in test_arrays:
-    if check_permutation(item[0],item[1]):
+    if check_substring(item[0],item[1]):
         print (item)
         print ("has permutation")
     else:
