5830e9c073fd601946d5ab5655c7e8c6be38b5db
Don <cd155@163.com>
2020-08-05 15:41:29 +0800

    Change test function to unit test

--- 1_Arrays_and_Strings/1_1_Is_Unique.py
+++ 1_Arrays_and_Strings/1_1_Is_Unique.py
@@ -6,10 +6,10 @@ def is_unique_hash(test_string):
     dict_table = {}
     for char in test_string:
         if char in dict_table:
-            return True
+            return False
         else:
             dict_table[char] = True
-    return False
+    return True
 
 def is_unique_array(test_string):
     bool_array = [False] * 128
@@ -19,25 +19,43 @@ def is_unique_array(test_string):
             return False
 
         if bool_array[ord(char)]:
-            return True
+            return False
         else:
             bool_array[ord(char)] = True
-    return False
+    return True
 
 def is_unique_bit_array(test_string):
     # check the following if you have some troubles to understand the bit vector solution
     # https://stackoverflow.com/questions/9141830/explain-the-use-of-a-bit-vector-for-determining-if-all-characters-are-unique
     pass
 
-# for test only
-test_array = ["algorithm", "", "additional", "h", "2020", "DonaldTrump", "Tt", "12ad好"]
-for test_string in test_array:
-    if is_unique_hash(test_string):
-        print("Hash: Find the duplicate in " + test_string)
-    else:
-        print("Hash: No duplicate in " + test_string)
+import unittest
 
-    if is_unique_array(test_string):
-        print("Array: Find the duplicate in " + test_string)
-    else:
-        print("Array: No duplicate in " + test_string)
+class TestIsUniqueCase(unittest.TestCase):
+    def setUp(self):
+        self.sample_is_unique = ["algorithm"]#, "", "h", "DonaldTrump", "Tt", "12ad好"]
+        self.sample_is_not_unique = ["aa"]#"additional", "2020", "12zz明", "listening"]
+
+    def tearDown(self):
+        self.sample_is_unique = None
+        self.sample_is_not_unique = None
+
+    def test_is_unique_hash_true(self):
+        for item in self.sample_is_unique:
+            self.assertTrue(is_unique_hash(item))
+    
+    def test_is_unique_hash_false(self):
+        for item in self.sample_is_not_unique:
+            self.assertFalse(is_unique_hash(item))
+    
+    def test_is_unique_array_true(self):
+        for item in self.sample_is_unique:
+            self.assertTrue(is_unique_array(item))
+    
+    def test_is_unique_array_false(self):
+        for item in self.sample_is_not_unique:
+            self.assertFalse(is_unique_array(item))
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestIsUniqueCase)
+    unittest.TextTestRunner(verbosity=2).run(suite)
