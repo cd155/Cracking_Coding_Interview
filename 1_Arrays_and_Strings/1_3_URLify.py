0f572b11dddeae3bcdc37c6956b33ace53b9709d
Don <cd155@163.com>
2021-02-13 19:43:29 +0800

    more test case

--- 1_Arrays_and_Strings/1_3_URLify.py
+++ 1_Arrays_and_Strings/1_3_URLify.py
@@ -12,12 +12,12 @@ def replace_spaces_expression(input_string):
 
     # replace last whitespace to match the exmaple
     match_pattern = '([^ ]*) * $'
-    replacement_pattern = '\g<1>'
+    replacement_pattern = r'\g<1>'
     input_string = re.sub(match_pattern, replacement_pattern, input_string)
 
     # replace all single or more spaces with one %20
     match_pattern = '([^ ]*) +'
-    replacement_pattern = '\g<1>%20'
+    replacement_pattern = r'\g<1>%20'
     return re.sub(match_pattern, replacement_pattern, input_string)
 
 from pytest import *
@@ -26,22 +26,45 @@ class TestReplaceSpaces:
 
     # initialize an instance of Set for each test
     def setup_method(self, method):
-        self.test_input = "Mr  John Smith    " 
-        self.test_output = "Mr%20John%20Smith"
+        self.test_input1 = "Mr  John Smith    "
+        # the string in triple quote has difference with different indents
+        # remove indent to make sure string equal
+        self.test_input2 = """Mr  John Smith    
+Mrs  John Smith     
+expression   this   """
+        self.test_output1 = "Mr%20John%20Smith"
+        self.test_output2 = """Mr%20John%20Smith%20
+Mrs%20John%20Smith%20
+expression%20this"""
 
     # reset state variables
     def teardown_method(self, method):
-        self.test_input = None
-        self.test_output = None
+        self.test_input1 = None
+        self.test_input2 = None
+        self.test_output1 = None
+        self.test_output2 = None
 
     def test_replace_spaces_loop(self):
-        assert replace_spaces_expression(self.test_input) == self.test_output
+        assert replace_spaces_expression(self.test_input1) == self.test_output1
+        assert replace_spaces_expression(self.test_input2) == self.test_output2
 
-# test_input = "Mr  John Smith    " 
+# test_input = """Mr  John Smith     
+# Mrs  John Smith     
+# expression   this   """
+# test_input3 = """Mr  John Smith     
+# Mrs  John Smith     
+# expression   this   """
+
+# print(test_input == test_input3)
+
 # match_pattern = '([^ ]*) * $'
-# replacement_pattern = '\g<1>'
+# replacement_pattern = r'\g<1>'
 # result1 = re.sub(match_pattern, replacement_pattern, test_input)
 # match_pattern = '([^ ]*) +'
+# replacement_pattern = r'\g<1>%20'
 # result2 = re.sub(match_pattern, replacement_pattern, result1)
 # print(result1)
+# print(result2 == """Mr%20John%20Smith%20
+# Mrs%20John%20Smith%20
+# expression%20this""")
 # print(result2)
