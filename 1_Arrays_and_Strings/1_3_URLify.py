7af7bc9f7370a9309cb8ef2bd0d6112cf2831ef4
Don <cd155@163.com>
2021-02-13 18:07:24 +0800

    use expression

--- 1_Arrays_and_Strings/1_3_URLify.py
+++ 1_Arrays_and_Strings/1_3_URLify.py
@@ -6,13 +6,42 @@
 # Input: "Mr  John Smith    ", 13 
 # Output: "Mr%20John%20Smith"
 
-def replace_spaces(test_string):
-    has_space = False
+import re
 
-    for char in test_string:
-        if char == " " and has_space:
-            has_space = True
-            test_string.replace(" ", "%20", 1)
-        else:
-            has_space = False
-    return test_string
+def replace_spaces_expression(input_string):
+
+    # replace last whitespace to match the exmaple
+    match_pattern = '([^ ]*) * $'
+    replacement_pattern = '\g<1>'
+    input_string = re.sub(match_pattern, replacement_pattern, input_string)
+
+    # replace all single or more spaces with one %20
+    match_pattern = '([^ ]*) +'
+    replacement_pattern = '\g<1>%20'
+    return re.sub(match_pattern, replacement_pattern, input_string)
+
+from pytest import *
+
+class TestReplaceSpaces:
+
+    # initialize an instance of Set for each test
+    def setup_method(self, method):
+        self.test_input = "Mr  John Smith    " 
+        self.test_output = "Mr%20John%20Smith"
+
+    # reset state variables
+    def teardown_method(self, method):
+        self.test_input = None
+        self.test_output = None
+
+    def test_replace_spaces_loop(self):
+        assert replace_spaces_expression(self.test_input) == self.test_output
+
+# test_input = "Mr  John Smith    " 
+# match_pattern = '([^ ]*) * $'
+# replacement_pattern = '\g<1>'
+# result1 = re.sub(match_pattern, replacement_pattern, test_input)
+# match_pattern = '([^ ]*) +'
+# result2 = re.sub(match_pattern, replacement_pattern, result1)
+# print(result1)
+# print(result2)
