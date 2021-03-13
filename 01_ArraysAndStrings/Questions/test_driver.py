e0ea4fc3327cf6ba72dfa7557232a8f82c00aeb2
Don <cd155@163.com>
2021-03-13 14:34:28 +0800

    add TestFindStringCompression

--- 01_ArraysAndStrings/Questions/test_driver.py
+++ 01_ArraysAndStrings/Questions/test_driver.py
@@ -2,6 +2,7 @@ from pytest import *
 from URLify import replace_spaces_expression
 from PalindromePermutation import check_permutation_of_palindrome_with_pre_process
 from OneAway import find_if_one_edit_away
+from StringCompression import find_string_compression
 
 
 # URLify
@@ -15,21 +16,17 @@ class TestReplaceSpaces:
         self.test_input2 = """Mr  John Smith    
 Mrs  John Smith     
 expression   this   """
-        self.test_output1 = "Mr%20John%20Smith"
-        self.test_output2 = """Mr%20John%20Smith%20
-Mrs%20John%20Smith%20
-expression%20this"""
 
     # reset state variables
     def teardown_method(self, method):
         self.test_input1 = None
         self.test_input2 = None
-        self.test_output1 = None
-        self.test_output2 = None
 
     def test_replace_spaces_loop(self):
-        assert replace_spaces_expression(self.test_input1) == self.test_output1
-        assert replace_spaces_expression(self.test_input2) == self.test_output2
+        assert replace_spaces_expression(self.test_input1) == "Mr%20John%20Smith"
+        assert replace_spaces_expression(self.test_input2) == """Mr%20John%20Smith%20
+Mrs%20John%20Smith%20
+expression%20this"""
 
 
 # Palindrome Permutation
@@ -47,7 +44,7 @@ class TestCheckPermutationOfPalindrome:
         self.input2 = None
         self.input3 = None
 
-    def test_replace_spaces_loop(self):
+    def test_check_permutation_of_palindrome(self):
         assert check_permutation_of_palindrome_with_pre_process(self.input1)
         assert check_permutation_of_palindrome_with_pre_process(self.input2)
         assert not check_permutation_of_palindrome_with_pre_process(self.input3)
@@ -74,10 +71,28 @@ class TestFindIfOneEditAway:
         self.input5 = None
         self.input6 = None
 
-    def test_replace_spaces_loop(self):
+    def test_find_if_one_edit_away(self):
         assert find_if_one_edit_away(self.input1[0], self.input1[1])
         assert find_if_one_edit_away(self.input2[0], self.input2[1])
         assert find_if_one_edit_away(self.input3[0], self.input3[1])
         assert not find_if_one_edit_away(self.input4[0], self.input4[1])
         assert not find_if_one_edit_away(self.input5[0], self.input5[1])
         assert not find_if_one_edit_away(self.input6[0], self.input6[1])
+
+
+# String Compression
+class TestFindStringCompression:
+
+    # initialize an instance of Set for each test
+    def setup_method(self, method):
+        self.input1 = "aabcccccaaa"
+        self.input2 = "abcd"
+
+    # reset state variables
+    def teardown_method(self, method):
+        self.input1 = None
+        self.input2 = None
+
+    def test_find_string_compression(self):
+        assert find_string_compression(self.input1) == "a2b1c5a3"
+        assert find_string_compression(self.input2) == "abcd"
