78c79132d7d3460fce796d5971bf1ee8129a1227
cd155 <github@donchen.page>
2021-10-22 17:38:07 -0400

    add queue in haskell

---
+++ 03_StacksAndQueues/Queue.hs
@@ -0,0 +1,29 @@
+module Structure.Queue where
+
+newtype Queue a = QueueD [a] deriving (Show)  
+
+-- Initialize: create a new Queue
+newQueue :: Queue a
+newQueue = QueueD []
+
+-- Add: add an element at the end of the queue
+-- Test: add "!" (add "world" (add "hi" newQueue))
+add :: a -> Queue a -> Queue a
+add a (QueueD x) = QueueD (x ++ [a])
+
+-- Remove: remove an element at the top of the queue
+-- Test: remove (add "!" (add "world" (add "hi" newQueue)))
+remove :: Queue a -> Maybe (Queue a)
+remove (QueueD []) = Nothing
+remove (QueueD (x : y)) = Just (QueueD y)
+
+-- Peek: check the first of element in the queue
+-- Test: peekQueue (add "!" (add "world" newQueue))
+peekQueue :: Queue a -> Maybe a
+peekQueue (QueueD []) = Nothing
+peekQueue (QueueD (x : _)) = Just x
+
+-- Is Empty: check the queue whether it is empty
+isQueueEmpty :: Queue a -> Bool
+isQueueEmpty (QueueD []) = True 
+isQueueEmpty _ = False
