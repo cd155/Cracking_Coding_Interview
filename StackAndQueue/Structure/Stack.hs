c6e9b9aa57f822997dd2b2791af380d208a9fcdc
cd155 <github@donchen.page>
2021-10-30 20:38:58 -0400

    change y to xs

--- StackAndQueue/Structure/Stack.hs
+++ StackAndQueue/Structure/Stack.hs
@@ -1,6 +1,6 @@
 module StackAndQueue.Structure.Stack where
 
-newtype Stack a = StackD [a] deriving (Show)  
+newtype Stack a = StackD [a] deriving (Show)
 
 -- Initialize: create a new Stack
 newStack :: Stack a
@@ -10,20 +10,20 @@ newStack = StackD []
 -- Test: pop (push "hi" (push "world" (push "!" newStack)))
 pop :: Stack a -> (Stack a, Maybe a)
 pop (StackD []) = (StackD [], Nothing)
-pop (StackD (x : y)) = (StackD y, Just x)
+pop (StackD (x : xs)) = (StackD xs, Just x)
 
 -- Push: insert an element at the beginning of the stack
 -- Test: push "hi" (push "world" (push "!" newStack))
 push :: a -> Stack a -> Stack a
-push a (StackD x) = StackD (a : x) 
+push a (StackD x) = StackD (a : x)
 
 -- Peek: look up the first element of the stack
 -- Test: peekStack (push "world" (push "!" newStack))
 peekStack :: Stack a -> Maybe a
 peekStack (StackD []) = Nothing
-peekStack (StackD (x : y)) = Just x
+peekStack (StackD (x : _)) = Just x
 
 -- Is Empty: check whether the stack is empty
 isStackEmpty :: Stack a -> Bool
-isStackEmpty (StackD []) = True 
-isStackEmpty _ = False 
+isStackEmpty (StackD []) = True
+isStackEmpty _ = False
