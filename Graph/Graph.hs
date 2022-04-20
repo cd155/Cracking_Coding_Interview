4a14f463a2d7a5ee449415306d8b12be474adac4
cd155 <github@donchen.page>
2022-04-19 22:31:54 -0400

    rename value constructor

--- Graph/Graph.hs
+++ Graph/Graph.hs
@@ -2,15 +2,11 @@ module Graph.Graph where
 
 import Data.Set (Set, member)
 
-data Graph = Simple1{
-    vertices1 :: Set Vertex,
-    edges1 :: Set Edge
-}
+newtype Graph = G (Set Vertex, Set Edge)
 
-data WGraph = Simple2{
-    vertices2 :: Set Vertex,
-    edges2 :: Set WEdge
-}
+newtype WGraph = WG (Set Vertex,  Set WEdge)
+
+newtype WGraphSet = WGS (Set (Set Vertex, Set WEdge))
 
 data Vertex = One
             | Two
@@ -25,8 +21,8 @@ data Vertex = One
 -- Declaration: WGraphT : set ❰ set t , ❰ t, t ❱ ↔ ℝ ❱
 newtype Edge = E (Vertex, Vertex) deriving (Eq, Ord)
 
--- Declaration: WGraphT : set ❰ set t , ❰ t, t ❱ ↔ ℝ ❱
-newtype WEdge = WE ((Vertex, Vertex), Float) deriving (Eq, Ord)
+-- Declaration: WGraphT : set ❰ set t , ❰ t, t ❱ ↔ ℝ ❱It s
+newtype WEdge = WE ((Vertex, Vertex), Int) deriving (Eq, Ord)
 
 -- Declaration: isPath : ❰ set t, set ❰ t, t ❱ ❱ → Seq ❰ t, t ❱ → 𝔹
 -- isPath G 𝜖
@@ -47,15 +43,30 @@ isWPath _ [x]                                = True
 isWPath g (WE((_, b), w1):WE((c, d), w2):xs) = b == c && isWPath g (WE((c, d), w2):xs)
 
 -- | weight function for WEdge
-weight :: WEdge -> Float
-weight (WE ((_, _), x)) = x
+weightOfE :: WEdge -> Int
+weightOfE (WE ((_, _), x)) = x
 
+-- | weight function for WEdge
+weightOfSeqE :: [WEdge] -> Int
+weightOfSeqE es = sum $ map weightOfE es
+
+-- Declaration: isPathValid : ❰ set t, set ❰ t, t ❱ ❱ → Seq ❰ t, t ❱ → 𝔹
+-- isPathValid G 𝜖
+-- isPathValid ⟨V, E⟩ (e ◃ 𝜖) ≡ e ∈ E
+-- isPathValid ⟨V, E⟩ (x ◃ xs) ≡ x ∈ E ∧ isPath ⟨V, E⟩ ( x ◃ xs)
 isPathValid :: Graph -> [Edge] -> Bool
 isPathValid _ []                 = True
-isPathValid (Simple1 _ e) [x]    = x `member` e
-isPathValid (Simple1 v e) (x:xs) = isPath (Simple1 v e) (x:xs) && x `member` e
+isPathValid (G (_, e)) [x]    = x `member` e
+isPathValid (G (v, e)) (x:xs) = x `member` e && isPath (G (v, e)) (x:xs)
 
 isWPathValid :: WGraph -> [WEdge] -> Bool
 isWPathValid _ []                 = True
-isWPathValid (Simple2 _ e) [x]    = x `member` e
-isWPathValid (Simple2 v e) (x:xs) = isWPath (Simple2 v e) (x:xs) && x `member` e
+isWPathValid (WG (_, e)) [x]    = x `member` e
+isWPathValid (WG (v, e)) (x:xs) = x `member` e && isWPath (WG (v, e)) (x:xs)
+
+-- findShortestPath :: WGraph -> Int
+-- findShortestPath (Simple2 v e) = powerSet([])
+
+-- Declaration: Paths : ❰ set t, set ❰ t, ℝ, t ❱ ❱ → set (Seq ❰ t, ℝ, t ❱)
+allPaths :: WGraph -> Set [Edge]
+allPaths (WG (v, e)) =  (WG(v, e)) `member` WGraphSet -- stop here.
