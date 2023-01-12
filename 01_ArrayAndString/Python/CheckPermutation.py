a109ce329c48ffb64e319a80172ef88113726b56
cd155 <github@postmail.app>
2023-01-12 13:56:20 -0500

    make code more readable

--- 01_ArrayAndString/Python/CheckPermutation.py
+++ 01_ArrayAndString/Python/CheckPermutation.py
@@ -1,34 +1,35 @@
 # # Check Permutation
 # 
-#     Given two strings, write a method to decide if one is a permutation of the other.
+# Given two strings, write a method to decide if one is a 
+# permutation of the other.
 #     
-#     A Permutation of a string: another string that contains same characters, but they can have different orders, only the order of characters can be different. For example, “abcd” and “dabc” are Permutation of each other.
+# A Permutation of a string: another string that contains same 
+# characters, but they can have different orders, only the order 
+# of characters can be different. For example, “abcd” and “dabc” 
+# are Permutation of each other.
 #     
-#     Please ask interviewer about whether blank spaces or case sensitive need to be considered. "dog" and "god" or "Dog" and "dog"
-
+# Please ask interviewer about whether blank spaces or case 
+# sensitive need to be considered. "dog" and "god" or "Dog" and "dog"
 
 def is_permute_dict(input1, input2):
-    if len(input1) != len(input2):
-        return False
-    dict1 = {}
-    dict2 = {}
-    for i in range(len(input1)):
-        if input1[i] in dict1:
-            dict1[input1[i]] += 1
-        else:
-            dict1[input1[i]] = 1
-        if input2[i] in dict2:
-            dict2[input2[i]] += 1
-        else:
-            dict2[input2[i]] = 1
+    dict1 = convToDict(input1, {})
+    dict2 = convToDict(input2, {})
     return dict1 == dict2
 
+def convToDict(s, dict):
+    for ele in s:
+        if ele in dict: 
+            dict[ele] += 1
+        else:
+            dict[ele] = 1
+    return dict
+
 def is_permute_arr(input1, input2):
-    if len(input1) != len(input2):
-        return False
-    arr1 = [0]*128
-    arr2 = [0]*128
-    for i in range(len(input1)):
-        arr1[ord(input1[i])] += 1
-        arr2[ord(input2[i])] += 1
-    return arr1 == arr2
+    dict1 = convToArray(input1, [0]*128)
+    dict2 = convToArray(input2, [0]*128)
+    return dict1 == dict2
+
+def convToArray(s, arr):
+    for ele in s:
+        arr[ord(ele)] += 1
+    return arr
